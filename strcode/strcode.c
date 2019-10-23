591272a52d13e06abfba1f1fd52fd10f69eb03bb
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-10-23 02:05:28 -0400

    explicit truth eval to suppress -Wall

--- strcode/strcode.c
+++ strcode/strcode.c
@@ -10,7 +10,7 @@ unsigned int StrCode16( char *string )
 	unsigned char *p = (unsigned char *)string;
 	unsigned short id = 0;
 	
-	while ( c = *p++ )
+	while (( c = *p++ ))
 	{
 		id = (( id >> 11 ) | ( id << 5 ));
 		id += c;
@@ -25,7 +25,7 @@ unsigned int StrCode24( char *string )
 	unsigned char *p = (unsigned char *)string;
 	unsigned int id = 0, mask = 0x00FFFFFF;
 	
-	while ( c = *p++ )
+	while (( c = *p++ ))
 	{
 		id = (( id << 5 ) | ( id >> 19 ));
 		id += c;
@@ -41,7 +41,7 @@ unsigned int StrCode32( char *string )
 	signed   int n  = 0;
 	unsigned int id = 0;
 	
-	while ( c = *string++ )
+	while (( c = *string++ ))
 	{
 		id += (( id << ( c & 0x0F )) | (( id >> 3 ) + ( c << ( n & 0x0F )) + c ));
 		n++;
